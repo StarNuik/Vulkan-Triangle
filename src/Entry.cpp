a2e51d425d89d635b7d90a8a5491c7e7ad6101c9
StarNuik <starnuik@gmail.com>
2020-06-07 18:44:06 +0300

    Logical device creation v0.5

--- src/Entry.cpp
+++ src/Entry.cpp
@@ -41,7 +41,31 @@ private:
 		CreateLogicalDevice();
 	}
 
-	void CreateLogicalDevice() {}
+	void CreateLogicalDevice() {
+		QueueFamilyIndices indices = FindQueueFamilies(physicalDevice);
+
+		VkDeviceQueueCreateInfo queueCreateInfo{};
+		queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+		queueCreateInfo.queueCount = 1;
+		float queuePriority = 1.0;
+		queueCreateInfo.pQueuePriorities = &queuePriority;
+
+		VkPhysicalDeviceFeatures deviceFeatures{};
+
+		VkDeviceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+		createInfo.pQueueCreateInfos = &queueCreateInfo;
+		createInfo.queueCreateInfoCount = 1;
+		createInfo.pEnabledFeatures = &deviceFeatures;
+		//! I STOPPED HERE
+		//!
+		//!
+		//!
+		//!
+		//!
+		//! HEEEEREEEEE
+	}
 
 	void PickPhysicalDevice() {
 		uint deviceCount = 0;
